45d552e06bd8f05458dc6e430cc41b8f4f06ddd5
NathaNascimento <nathansantos3211@gmail.com>
2019-03-26 11:42:10 -0300

    no message

---
+++ Atividade01/Fontes/Paula01.dpr
@@ -0,0 +1,14 @@
+program Paula01;
+
+uses
+  Vcl.Forms,
+  UAula01 in '..\UAula01.pas' {FAula01};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFAula01, FAula01);
+  Application.Run;
+end.
